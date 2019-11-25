714118f4c00cc0f2a9b5c7340ccbd377bd1bb4e4
Yash Kumar Arora <arorayash1998@gmail.com>
2019-11-25 22:47:00 +0530

    Updated some Styles

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 
 var typed = new Typed('#tag-line', {
     strings: ["Developer.^500 Dreamer.^500 Achiever. ^500;^100)"],
-    typeSpeed: 10,
+    typeSpeed: 15,
     startDelay: 1000,
     fadeOut: true,
     fadeOutClass: 'typed-fade-out',
